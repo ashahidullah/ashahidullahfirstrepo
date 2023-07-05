5f520c7c83ba952fc8cdaf3bc72f04e978c1c068
ashahidullah <138694327+ashahidullah@users.noreply.github.com>
2023-07-05 21:07:59 +0600

    Add files via upload
    Its a test

---
+++ hello.py
@@ -0,0 +1,24 @@
+text = "Hello world"
+print(text)
+number1 = 10
+number2 = 20
+result = number1+number2
+print(result)
+number1 =16
+print(number1)
+number1 =16
+result = number1/number2
+print(result)
+number1 = "Hi all"
+print(number1)
+print(number1,number2)
+numbers = [1,2,3,4,5]
+print(numbers)
+print(numbers[4])
+print(numbers[0])
+saarc =["BD","IN","PK","SR","NP","BH","MD","AF"]
+print(saarc)
+print("My country: ",saarc[0])
+
+
+
